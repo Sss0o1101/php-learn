136e7d0ad97c31456aebaf4d9e2198a8a73ee350
sss <matusoyo2436@gmail.com>
2024-09-09 20:36:27 +0900

    added text
    echo 1.2e-3;
    
    // 数値に対して使える、さまざまな演算子
    <?php
    echo 10 + 3;
    echo 10 - 3;
    echo 10 * 3;
    echo 10 ** 3;
    echo 10 / 3;
    echo 10 % 3;
    echo 10 + 2 * 3;
    echo (10 + 2) * 3;

--- index.php
+++ index.php
@@ -9,3 +9,14 @@ echo 2.5;
 echo 1_000_000;
 echo 1.2e3;
 echo 1.2e-3;
+
+// 数値に対して使える、さまざまな演算子
+<?php
+echo 10 + 3;
+echo 10 - 3;
+echo 10 * 3;
+echo 10 ** 3;
+echo 10 / 3;
+echo 10 % 3;
+echo 10 + 2 * 3;
+echo (10 + 2) * 3;
