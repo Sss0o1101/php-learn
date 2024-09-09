9de592ca4d45f702bed2851ce18ef464073b1ac7
sss <matusoyo2436@gmail.com>
2024-09-09 21:01:39 +0900

    added text
    // PHPにおける定数
    <?php
    $price = 150;
    // $rate = 1.1;
    const RATE = 1.1;
    echo $price * 120 * RATE;
    echo $price * 130 * RATE;
    echo $price * 140 * RATE;
    
    $price++;
    RATE = 1.08;
    echo $price * 120 * RATE;
    echo $price * 130 * RATE;
    echo $price * 140 * RATE;

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!-- 文字列 -->
 <?php echo "Hello world";?> 
 
+
+
 <!-- 数値のさまざまな表現方法 -->
 <?php
 echo 10;
@@ -9,6 +11,8 @@ echo 2.5;
 echo 1_000_000;
 echo 1.2e3;
 echo 1.2e-3;
+
+
 
 // 数値に対して使える、さまざまな演算子
 <?php
@@ -21,6 +25,8 @@ echo 10 % 3;
 echo 10 + 2 * 3;
 echo (10 + 2) * 3;
 
+
+
 // PHPにおける変数
 <?php
 echo 150 * 120;
@@ -32,8 +38,9 @@ echo $price * 120;
 echo $price * 130;
 echo $price * 140;
 
-// 変数に値を再代入
 
+
+// 変数に値を再代入
 <?php
 $price = 150;
 echo $price * 120 . PHP_EOL;
@@ -48,3 +55,20 @@ $price++;
 // $price--;
 echo $price * 120;
 echo $price * 130 . PHP
+
+
+
+// PHPにおける定数
+<?php
+$price = 150;
+// $rate = 1.1;
+const RATE = 1.1;
+echo $price * 120 * RATE;
+echo $price * 130 * RATE;
+echo $price * 140 * RATE;
+
+$price++;
+RATE = 1.08;
+echo $price * 120 * RATE;
+echo $price * 130 * RATE;
+echo $price * 140 * RATE;
