4f1b547d72b86c9e5c0574ef8d4d502dbd8fea77
sss <matusoyo2436@gmail.com>
2024-09-10 18:51:34 +0900

    added text
    //スコアが 90 点以上だったら、A
    
    // PHPにおける(else if)
    echo "Score? ";
    $score = (int) fgets(STDIN);
    
    if ($score >= 90) {
        echo "A";
    } else {
        echo "Not A";
    }
    //スコア が 90 点未満だったら、 A 判定じゃなかったという意味で Not A
    
    // PHPにおける、if else(信号機)
    <?php
    echo "Signal? ";
    $signal = trim(fgets(STDIN));    // 前後の空白や改行を除去するには trim()
    
    if ($signal === "red") {
        echo "Stop";
    } else if ($signal === "blue") {
        echo "Go";
    } else if ($signal === "yellow") {
        echo "Slow down";
    } else {
        echo "Wrong signal";
    }
    
    // PHPにおける、switchを使った条件分岐(信号機)
    <?php
    echo "Signal? ";
    $signal = trim(fgets(STDIN));
    
    switch ($signal) {
        case "red":
            echo "Stop";
            break;
        case "blue":
        case "green":
            echo "Go";
            break;
        case "yellow":
            echo "Slow down";
            break;
        default:
            echo "Wrong signal";
            break;
    }

--- php-L2(d)/index.php
+++ php-L2(d)/index.php
@@ -9,3 +9,57 @@ if ($score >= 90) {
     echo "Not A";
 }
 //スコアが 90 点以上だったら、A
+
+
+
+
+// PHPにおける(else if)
+echo "Score? ";
+$score = (int) fgets(STDIN);
+
+if ($score >= 90) {
+    echo "A";
+} else {
+    echo "Not A";
+}
+//スコア が 90 点未満だったら、 A 判定じゃなかったという意味で Not A
+
+
+
+// PHPにおける、if else(信号機)
+<?php
+echo "Signal? ";
+$signal = trim(fgets(STDIN));    // 前後の空白や改行を除去するには trim()
+
+if ($signal === "red") {
+    echo "Stop";
+} else if ($signal === "blue") {
+    echo "Go";
+} else if ($signal === "yellow") {
+    echo "Slow down";
+} else {
+    echo "Wrong signal";
+}
+
+
+
+// PHPにおける、switchを使った条件分岐(信号機)
+<?php
+echo "Signal? ";
+$signal = trim(fgets(STDIN));
+
+switch ($signal) {
+    case "red":
+        echo "Stop";
+        break;
+    case "blue":
+    case "green":
+        echo "Go";
+        break;
+    case "yellow":
+        echo "Slow down";
+        break;
+    default:
+        echo "Wrong signal";
+        break;
+}
